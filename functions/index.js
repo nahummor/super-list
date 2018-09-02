06b6719f66e4054a467693c026ffb2887c3fb0ed
nahummor <nahummor@gmail.com>
2018-09-02 21:54:36 +0300

    add fb functions

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
-// // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
+
 const cors = require('cors')({ origin: true });
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
