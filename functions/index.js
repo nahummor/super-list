5f170cdaee913b8484a9cb26b8ac22838ea606a1
nahummor <nahummor@gmail.com>
2018-09-02 21:52:48 +0300

    add fb functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,20 @@
-const functions = require('firebase-functions');
-
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const cors = require('cors')({ origin: true });
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+const serviceAccount = require('./serviceAccountKey.json');
+const nodemailer = require('nodemailer');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://superlist-80690.firebaseio.com'
+});
+
+const db = admin.firestore();
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  cors(request, response, () => {
+    response.status(200).json({ message: 'Hello World from Firebase' });
+  });
+});
