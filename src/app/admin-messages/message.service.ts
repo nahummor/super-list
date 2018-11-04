b4d05621ecb170ff4256c1ec2fe20c28873c0456
nahummor <nahummor@gmail.com>
2018-11-04 22:09:11 +0200

    finish admin msg box

--- src/app/admin-messages/message.service.ts
+++ src/app/admin-messages/message.service.ts
@@ -9,17 +9,31 @@ import { AngularFirestore } from '@angular/fire/firestore';
 export class MessageService {
   constructor(private db: AngularFirestore) {}
 
-  public getMessagesList(): Observable<{ id: string; message: string }[]> {
+  public getMessagesList(): Observable<
+    { id: string; msgNumber: number; message: string }[]
+  > {
+    const today = new Date();
     return this.db
-      .collection('admin-messages')
+      .collection('admin-messages', ref => {
+        return ref.where('expDate', '>', today);
+      })
       .get()
       .pipe(
         map(snapshot => {
           return snapshot.docs.map(doc => {
             return {
               id: doc.id,
+              msgNumber: doc.data().msgNumber,
               message: doc.data().message
             };
+          });
+        }),
+        map(messages => {
+          return messages.sort((num1, num2) => {
+            if (num1.msgNumber > num2.msgNumber) {
+              return 1;
+            }
+            return -1;
           });
         })
       );
