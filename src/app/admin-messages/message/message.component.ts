bbc7d03de0022e5e1b817e8ff531097dc74dd4a5
nahummor <nahummor@gmail.com>
2018-11-04 16:59:41 +0200

    add admin msg box

--- src/app/admin-messages/message/message.component.ts
+++ src/app/admin-messages/message/message.component.ts
@@ -1,3 +1,4 @@
+import { MatDialogRef } from '@angular/material';
 import { MessageService } from './../message.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,13 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessageComponent implements OnInit {
   public messages: any[];
+  public isDoneLoadingMessages: boolean;
 
-  constructor(private msgService: MessageService) {}
+  constructor(
+    private msgService: MessageService,
+    public dialogRef: MatDialogRef<MessageComponent>
+  ) {}
 
   ngOnInit() {
+    this.isDoneLoadingMessages = false;
     this.msgService.getMessagesList().subscribe(messages => {
       console.log(messages);
       this.messages = messages;
+      this.isDoneLoadingMessages = true;
     });
+  }
+
+  public onClose() {
+    this.dialogRef.close('close');
   }
 }
